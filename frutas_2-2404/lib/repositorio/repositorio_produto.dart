e89979dfc55d3d1adaa3d274df4b874e1383430e
daniykt <daniykt7@gmail.com>
2024-05-12 19:08:43 -0300

    quitanda Part2

---
+++ frutas_2-2404/lib/repositorio/repositorio_produto.dart
@@ -0,0 +1,50 @@
+import 'package:frutas_2/entidade/produto.dart';
+import 'package:uuid/uuid.dart';
+
+class RepositorioProduto {
+  var uuid = const Uuid();
+  List<Produto> select() {
+    return <Produto>[
+      Produto(
+          id: uuid.v4().toString(),
+          nome: "Melão Amarelo",
+          foto:
+              "https://api.quitandadivino.com.br/uploads/melao_1b0b490e4e.jpg",
+          categoria: "Fruta",
+          estoque: 10,
+          preco: 6.10),
+      Produto(
+          id: uuid.v4().toString(),
+          nome: "Morango",
+          foto:
+              "https://api.quitandadivino.com.br/uploads/morango_b81411108f.jpg",
+          categoria: "Fruta",
+          estoque: 10,
+          preco: 10.10),
+      Produto(
+          id: uuid.v4().toString(),
+          nome: "Uva Niágara",
+          foto:
+              "https://api.quitandadivino.com.br/uploads/uva_niagara_69c6a0188e.jpg",
+          categoria: "Fruta",
+          estoque: 10,
+          preco: 4.95),
+      Produto(
+          id: uuid.v4().toString(),
+          nome: "Pera Willians",
+          foto:
+              "https://api.quitandadivino.com.br/uploads/pera_williams_c9851fecec.jpg",
+          categoria: "Fruta",
+          estoque: 10,
+          preco: 3.10),
+      Produto(
+          id: uuid.v4().toString(),
+          nome: "Melancia Baby",
+          foto:
+              "https://api.quitandadivino.com.br/uploads/melancia_baby_566f69fe2c.jpg",
+          categoria: "Fruta",
+          estoque: 10,
+          preco: 10.10),
+    ];
+  }
+}
