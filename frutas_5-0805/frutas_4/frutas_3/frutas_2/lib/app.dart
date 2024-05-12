2c3d6d336426502667d02dd97bb0652d8a9e1ea4
daniykt <daniykt7@gmail.com>
2024-05-12 19:13:02 -0300

    quitanda Part5

---
+++ frutas_5-0805/frutas_4/frutas_3/frutas_2/lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:frutas_2/pages/carrinho._page.dart';
+import 'package:frutas_2/pages/detalhe_produto_page.dart';
+import 'package:frutas_2/pages/home_page.dart';
+import 'package:frutas_2/pages/login_page.dart';
+import 'package:frutas_2/util/tema.dart';
+
+class Aplicacao extends StatelessWidget {
+  const Aplicacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: temaPadrao(),
+      routes: {
+        "/login": (context) => const LoginPage(),
+        "/": (context) => const HomePage(),
+        "/detail": (context) => const DetalheProdutoPage(),
+        "/carrinho": (context) => const CarrinhoPage(),
+      },
+      initialRoute: "/",
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
