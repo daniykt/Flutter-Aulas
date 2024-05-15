e2668c0bd418b6ae1a2419e3fae66f15f3d18842
daniykt <daniykt7@gmail.com>
2024-05-15 20:50:21 -0300

    Aula 1505

--- frutas_5-0805/frutas_4/frutas_3/frutas_2/lib/app.dart
+++ frutas_5-0805/frutas_4/frutas_3/frutas_2/lib/app.dart
@@ -3,6 +3,7 @@ import 'package:frutas_2/pages/carrinho._page.dart';
 import 'package:frutas_2/pages/detalhe_produto_page.dart';
 import 'package:frutas_2/pages/home_page.dart';
 import 'package:frutas_2/pages/login_page.dart';
+import 'package:frutas_2/pages/perfil_page.dart';
 import 'package:frutas_2/util/tema.dart';
 
 class Aplicacao extends StatelessWidget {
@@ -13,10 +14,11 @@ class Aplicacao extends StatelessWidget {
     return MaterialApp(
       theme: temaPadrao(),
       routes: {
-        "/login": (context) => const LoginPage(),
         "/": (context) => const HomePage(),
         "/detail": (context) => const DetalheProdutoPage(),
         "/carrinho": (context) => const CarrinhoPage(),
+        "/login": (context) => const LoginPage(),
+        "/perfil": (context) => const PerfilPage(),
       },
       initialRoute: "/login",
       debugShowCheckedModeBanner: false,
