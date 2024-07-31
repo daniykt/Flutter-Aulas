96675be65801c65b69d12731158f9732e3315daf
daniykt <daniykt7@gmail.com>
2024-07-31 16:43:17 -0300

    Comentando código

--- PM-II/cinemameu/lib/componentes/cinema_item.dart
+++ PM-II/cinemameu/lib/componentes/cinema_item.dart
@@ -1,20 +1,27 @@
 import 'package:cinemameu/entidade/cinema.dart';
 import 'package:flutter/material.dart';
 
+// Define um widget chamado 'CinemaItem', que é um widget sem estado (StatelessWidget)
 class CinemaItem extends StatelessWidget {
+  // Declara uma variável final do tipo 'Cinema' chamada 'cinema'
   final Cinema cinema;
 
-  const CinemaItem({super.key
-  , 
-  required this.cinema,
+  // Construtor da classe 'CinemaItem', que recebe uma chave opcional e um objeto 'cinema' obrigatório
+  const CinemaItem({
+    super.key, // Chave opcional que pode ser usada para identificar o widget
+    required this.cinema, // Parâmetro obrigatório que deve ser passado ao criar um 'CinemaItem'
   });
 
+  // Método 'build' que constrói a interface do usuário do widget
   @override
   Widget build(BuildContext context) {
     return Container(
+      // Define uma margem de 5 pixels em torno do container
       margin: const EdgeInsets.all(5),
       child: Material(
+        // Define uma elevação de 5 pixels para o efeito de sombra
         elevation: 5,
+        // Exibe o nome do cinema dentro de um widget de texto
         child: Text(cinema.nome),
       ),
     );
--- PM-II/cinemameu/lib/repositorio/repositorio_cinema.dart
+++ PM-II/cinemameu/lib/repositorio/repositorio_cinema.dart
@@ -1,22 +1,29 @@
 import 'package:cinemameu/entidade/cinema.dart';
 
+// Define uma classe chamada 'RepositorioCinema' que atua como um repositório de cinemas
 class RepositorioCinema {
+  // Método 'select' que retorna uma lista de objetos 'Cinema'
   List<Cinema> select() {
+    // Retorna uma lista de cinemas predefinidos
     return <Cinema>[
+      // Instância do primeiro cinema com suas propriedades definidas
       Cinema(
-          nome: "Cine Teatro Matão",
-          latitude: -21.6015644,
-          longitude: -48.3688865,
-          endereco: "Rua João Pessoa, 842 - Centro",
-          telefone: "1633847777",
-          salas: 2),
+        nome: "Cine Teatro Matão", 
+        latitude: -21.6015644, 
+        longitude: -48.3688865, 
+        endereco: "Rua João Pessoa, 842 - Centro", 
+        telefone: "1633847777", 
+        salas: 2, 
+      ),
+      // Instância do segundo cinema com suas propriedades definidas
       Cinema(
-          nome: "Cinemec Taquaritinga",
-          latitude: -21.4057353,
-          longitude: -48.5079264,
-          endereco: "Rua da República, 929",
-          telefone: "1633847778",
-          salas: 1),
+        nome: "Cinemec Taquaritinga", 
+        latitude: -21.4057353, 
+        longitude: -48.5079264, 
+        endereco: "Rua da República, 929", 
+        telefone: "1633847778",
+        salas: 1, 
+      ),
     ];
   }
 }
